29eed0e815e67e3cde8dd34832ac9689419136da
LeonardoMaiaDimas <leonardodimasmaia@gmail.com>
2021-09-08 17:11:59 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,10 +3,14 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
+    private static Object Livro;
+
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro = new Livro("O Hobbit", 350);
+        System.out.println(Livro);
 
         /*int a= 5;
         int b= 3;
@@ -15,3 +19,38 @@ public class PrimeiroPrograma {
 
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
+    }
+}
+
